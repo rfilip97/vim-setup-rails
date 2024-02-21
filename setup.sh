c8d4aac8defa038320b9dd7e6afe1f13660222d4
Razvan Filip <razvan.filip.70@gmail.com>
2024-02-21 22:16:02 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Overwrite vim config
-cp .vimrc ~/.vimrc
+# Remove existing .vimrc if it exists
+if [ -f ~/.vimrc ] || [ -L ~/.vimrc ]; then
+  echo "Removing existing .vimrc"
+  rm ~/.vimrc
+fi
+
+# Create a symlink to .vimrc in the current directory
+echo "Creating a symlink to .vimrc in the current directory"
+ln -s "$(pwd)/.vimrc" ~/.vimrc
+chmod -x ~/.vimrc
+
+# Ensure clipboard support
+if ! vim --version | grep -q '+clipboard'; then
+  echo "Vim clipboard support not detected. Installing vim-gtk3..."
+  sudo apt update
+  sudo apt install -y vim-gtk3
+fi
 
 # Install Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  echo "Vim Plug is not installed. Installing..."
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # Install the plugins
 vim +PlugInstall +qall
